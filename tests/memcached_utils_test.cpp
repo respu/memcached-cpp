bc5b38d5c4395c9dce956eb56b9358719dcee72a
Stephan Dollberg <s.dollberg@tum.de>
2013-02-14 22:53:17 +0100

    added test for extract_datasize

--- tests/memcached_utils_test.cpp
+++ tests/memcached_utils_test.cpp
@@ -1,4 +1,19 @@
 #include "../include/memcached-cpp/detail/memcached_utils.hpp"
 #include "catch.hpp"
 
+#include "boost/asio.hpp"
+
+TEST_CASE("extract_datasize/1", "tests proper extraction of datasize") {
+    auto str = "VALUE rofl 0 1234\r\n";
+    boost::asio::streambuf test_buf;
+    std::ostream os(&test_buf);
+    os << str;
+
+    auto supposed = 1234;
+    auto test_val = memcachedcpp::detail::extract_datasize(test_buf);
+    CHECK(test_val == supposed);
+}
+
+
+
 
