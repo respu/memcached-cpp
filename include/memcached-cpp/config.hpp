6e85655e07c728febd7bd052c72327958ae42e77
Stephan Dollberg <s.dollberg@tum.de>
2013-03-04 16:53:25 +0100

    added config.hpp

---
+++ include/memcached-cpp/config.hpp
@@ -0,0 +1,12 @@
+// (C) Copyright Stephan Dollberg 2013. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#ifndef MEMCACHED_CONFIG_HPP
+#define MEMCACHED_CONFIG_HPP
+
+namespace memcachedcpp {
+    enum class ip { tcp, udp };
+    enum class protocol { plain, binary };
+}
+
+#endif // MEMCACHED_CONFIG_HPP
